e51fc15d706e935cfee25aacba6f31178dbe0f5f
Andrew Curtis <andrew@larrycurtis.com>
2024-02-29 13:18:13 -0600

    Initial react app

---
+++ src/Components/TemplateTest.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TemplateTest() {
+    return (
+        <div>
+            <h1>Page Title</h1>
+            { /* Add your content here */}
+        </div>
+    );
+}
+
+export default TemplateTest;

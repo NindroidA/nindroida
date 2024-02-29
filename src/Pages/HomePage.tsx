6a898374aec7031e8c7a0f96ce9e77102959de32
Andrew Curtis <andrew@larrycurtis.com>
2024-02-29 13:29:58 -0600

    adding routes

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -4,7 +4,7 @@ function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
-            <p>This is the home page.</p>
+            <p>This is the home page. More coming soon.</p>
         </div>
     );
 }
